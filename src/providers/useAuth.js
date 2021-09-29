91eb589caef4dd4766b2325dec1b945c43af45b8
Kuba <jakubkozlowski.pl@gmail.com>
2021-09-29 16:14:54 +0200

    Added endpoint to heroku server

--- src/providers/useAuth.js
+++ src/providers/useAuth.js
@@ -11,7 +11,7 @@ export const AuthProvider = ({ children }) => {
     if (token) {
       ;(async () => {
         try {
-          const response = await axios.get('http://localhost:8080/api/main', {
+          const response = await axios.get('https://notes-application-v1.herokuapp.com/api/main', {
             headers: {
               authorization: `Bearer ${token}`,
             },
@@ -26,7 +26,7 @@ export const AuthProvider = ({ children }) => {
 
   const signIn = async ({ login, password }) => {
     try {
-      const response = await axios.post('http://localhost:8080/api/login', {
+      const response = await axios.post('https://notes-application-v1.herokuapp.com/api/login', {
         login,
         password,
       })
